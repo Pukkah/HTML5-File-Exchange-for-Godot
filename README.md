4af11255416f2b8c05c9190e16f56a822c6a3636
yiyuezhuo <yiyuezhuo@gmail.com>
2022-05-08 01:11:47 +0800

    Refactor using better Godot and JavaScript API and add an example

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HTML5 File Exchange for Godot 3.2
+
+## Usage
+
+* Copy addons to your project.
+* Enable the plugin. (A `HTML5File` singleton should be added into autoload list).
+* Add a HTML5 export item to enable HTML5 debugging option.
+* Check sample.tscn for usage.
--- addons/HTML5FileExchange/HTML5FileExchange.gd
+++ addons/HTML5FileExchange/HTML5FileExchange.gd
@@ -1,24 +1,21 @@
 extends Node
 
-signal InFocus
+signal read_completed
+signal load_completed(image)
+
+var js_callback = JavaScript.create_callback(self, "load_handler");
+var js_interface;
 
 func _ready():
 	if OS.get_name() == "HTML5" and OS.has_feature('JavaScript'):
 		_define_js()
-
-
-func _notification(notification:int) -> void:
-	if notification == MainLoop.NOTIFICATION_WM_FOCUS_IN:
-		emit_signal("InFocus")
+		js_interface = JavaScript.get_interface("_HTML5FileExchange");
 
 func _define_js()->void:
 	#Define JS script
 	JavaScript.eval("""
-	var fileData;
-	var fileType;
-	var fileName;
-	var canceled;
-	function upload() {
+	var _HTML5FileExchange = {};
+	_HTML5FileExchange.upload = function(gd_callback) {
 		canceled = true;
 		var input = document.createElement('INPUT'); 
 		input.setAttribute("type", "file");
@@ -29,56 +26,32 @@ func _define_js()->void:
 				canceled = false;}
 			var file = event.target.files[0];
 			var reader = new FileReader();
-			fileType = file.type;
-			fileName = file.name;
+			this.fileType = file.type;
+			// var fileName = file.name;
 			reader.readAsArrayBuffer(file);
-			reader.onloadend = function (evt) {
+			reader.onloadend = (evt) => { // Since here's it's arrow function, "this" still refers to _HTML5FileExchange
 				if (evt.target.readyState == FileReader.DONE) {
-					fileData = evt.target.result;
+					this.result = evt.target.result;
+					gd_callback(); // It's hard to retrieve value from callback argument, so it's just for notification
 				}
 			}
 		  });
 	}
-	function download(fileName, byte) {
-		var buffer = Uint8Array.from(byte);
-		var blob = new Blob([buffer], { type: 'image/png'});
-		var link = document.createElement('a');
-		link.href = window.URL.createObjectURL(blob);
-		link.download = fileName;
-		link.click();
-	};
 	""", true)
+
+func load_handler(_args):
+	emit_signal("read_completed")
 	
-	
-func load_image()->Image:
+func load_image():
 	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
 		return
-		
-	#Execute js function
-	JavaScript.eval("upload();", true)	#opens promt for choosing file
+
+	js_interface.upload(js_callback);
+
+	yield(self, "read_completed")
 	
-	#label.text = "Wait for focus"
-	yield(self, "InFocus")	#wait until js promt is closed
-	
-	#label.text = "Timer on for loading"
-	yield(get_tree().create_timer(0.1), "timeout")	#give some time for async js data load
-	
-	if JavaScript.eval("canceled;", true):	# if File Dialog closed w/o file
-		#label.text = "Canceled prompt"
-		return
-	
-	# use data from png data
-	#label.text = "Load image"
-	var imageData
-	while true:
-		imageData = JavaScript.eval("fileData;", true)
-		if imageData != null:
-			break
-		#label.text = "No image yet"
-		yield(get_tree().create_timer(1.0), "timeout")	#need more time to load data
-	
-	var imageType = JavaScript.eval("fileType;", true)
-	var imageName = JavaScript.eval("fileName;", true)
+	var imageType = js_interface.fileType;
+	var imageData = JavaScript.eval("_HTML5FileExchange.result", true) # interface doesn't work as expected for some reason
 	
 	var image = Image.new()
 	var image_error
@@ -90,39 +63,18 @@ func load_image()->Image:
 		"image/webp":
 			image_error = image.load_webp_from_buffer(imageData)
 		var invalidType:
-			#label.text = "Unsupported file format - %s." % invalidType
+			print("Unsupported file format - %s." % invalidType)
 			return
+	
 	if image_error:
-		#label.text = "An error occurred while trying to display the image."
-		return
-	else:
-		return image
-		# Display texture
-		var tex = ImageTexture.new()
-		tex.create_from_image(image, 0) # Flag = 0 or else export is fucked!
-		Sprite.texture = tex
-		#loadedImage = image # Keep Image for later, just in case...
-		#loadedImageName = imageName
-		#label.text = "Image %s loaded as %s." % [imageName, imageType]
-		return
-	#label.text = "Something went wrong"
+		print("An error occurred while trying to display the image.")
+	
+	emit_signal("load_completed", image)
 
-
-func save_image(image:Image, fileName:String = "export")->void:
+func save_image(image:Image, fileName:String = "export.png")->void:
 	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
 		return
-		
+	
 	image.clear_mipmaps()
-	if image.save_png("user://export_temp.png"):
-		#label.text = "Error saving temp file"
-		return
-	var file:File = File.new()
-	if file.open("user://export_temp.png", File.READ):
-		#label.text = "Error opening file"
-		return
-	var pngData = Array(file.get_buffer(file.get_len()))	#read data as PoolByteArray and convert it to Array for JS
-	file.close()
-	var dir = Directory.new()
-	dir.remove("user://export_temp.png")
-	JavaScript.eval("download('%s', %s);" % [fileName, str(pngData)], true)
-	#label.text = "Saving DONE"
+	var buffer = image.save_png_to_buffer()
+	JavaScript.download_buffer(buffer, fileName)
